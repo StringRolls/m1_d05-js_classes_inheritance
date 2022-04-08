cf9e8984d302fbbba829f1a355cb7f56ddc6d7e4
TA WDFT BCN Ironhack <ta-web-bcn@ironhack.com>
2022-04-08 13:29:41 +0200

    ex1

--- exercises/01-exe-js_classes_inheritance.js
+++ exercises/01-exe-js_classes_inheritance.js
@@ -13,37 +13,14 @@
 */
 
 class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-  calculatePerimeter() {
-    return 2 * this.width + 2 * this.height;
-  }
-  calculateArea() {
-    return this.width * this.height;
-  }
+  // TODO
 }
 
 class Square extends Rectangle {
-  constructor(side) {
-    super(side, side);
-   // this.side = side; // you can ommit this
-  }
+  // TODO
 }
 
 const r1 = new Rectangle(6, 7);
-// The previous line is the same as
-// const r1 = {}
-// r1.width = 6
-// r1.height = 7
-// r1.calculatePerimeter = function() {
-//    return 2*this.width+2*this.height
-// }
-// r1.calculateArea = function() {
-//    return this.width * this.height
-// }
-
 console.log('Perimeter of r1 =', r1.calculatePerimeter()); // 26
 console.log('Area of r1 =', r1.calculateArea()); // 42
 
@@ -55,13 +32,6 @@ const s2 = new Square(10);
 console.log('Perimeter of s2 =', s2.calculatePerimeter()); // 40
 console.log('Area of s2 =', s2.calculateArea()); // 100
 
-
-// Perimeter of r1 = 26
-// Area of r1 = 42
-// Perimeter of s1 = 20
-// Area of s1 = 25
-// Perimeter of s2 = 40
-// Area of s2 = 100
-  /*
-  Note: Although the `square` class accepts just one parameter you have to pass `two` parameters while calling the `super` method ? Why ? Because as we saw in class the `super` method invokes/calls the parent class `constructor` method. And here the parent class is `rectangle`
-  */
+/*
+Note: Although the `square` class accepts just one parameter you have to pass `two` parameters while calling the `super` method ? Why ? Because as we saw in class the `super` method invokes/calls the parent class `constructor` method. And here the parent class is `rectangle`
+*/
